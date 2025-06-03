d47d624ec379248adb8138969c05b26617913bd1
Flavio Moreno <flaviomoreno321@gmail.com>
2025-06-02 22:49:39 -0300

    Implement authentication using NextAuth with custom configuration and credential provider
    - Added auth.config.ts to define NextAuth configuration including custom pages and callbacks.
    - Created auth.ts to handle user authentication with credentials, including user fetching and password verification.
    - Introduced middleware.ts to apply authentication middleware across the application, excluding specific routes.

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
-        { }
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -28,7 +27,6 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
           <Image src="/hero-mobile.png"
             alt="Screenshots of the dashboard project showing mobile version"
             width={560}
